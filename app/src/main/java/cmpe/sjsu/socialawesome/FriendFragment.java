b498eebac95d4bcabb9c7e2af0862e102d9e7de5
Bing Shi <sheilashi0112@gmail.com>
2017-05-11 16:12:14 -0700

    4. send friend request with null check, duplicate check, public profile check

--- app/src/main/java/cmpe/sjsu/socialawesome/FriendFragment.java
+++ app/src/main/java/cmpe/sjsu/socialawesome/FriendFragment.java
@@ -1,25 +1,13 @@
 package cmpe.sjsu.socialawesome;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
+import cmpe.sjsu.socialawesome.Utils.FriendUtils;
 import cmpe.sjsu.socialawesome.Utils.UserAuth;
 import cmpe.sjsu.socialawesome.models.UserSummary;
-
-import static cmpe.sjsu.socialawesome.StartActivity.USERS_TABLE;
-import static cmpe.sjsu.socialawesome.models.User.FOLLOWER_LIST;
-import static cmpe.sjsu.socialawesome.models.User.FOLOWING_FRIEND_LIST;
-import static cmpe.sjsu.socialawesome.models.User.FRIEND_LIST;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -39,68 +27,68 @@ public class FriendFragment extends SocialFragment {
     @Override
     public void onStart() {
         super.onStart();
-//        UserSummary mySummary = UserAuth.getInstance().getCurrentUserSummary();
-//        followPerson(mySummary);
+        UserSummary mySummary = UserAuth.getInstance().getCurrentUserSummary();
+        FriendUtils.addFriend(getActivity(), 0, mySummary);
     }
 
-    public void followPerson(final UserSummary summaryReceive) {
-        if (summaryReceive.status == 2) {
-            final DatabaseReference userTableRef = FirebaseDatabase.getInstance().getReference().child(USERS_TABLE);
-            final DatabaseReference currentUserRef = userTableRef.child(UserAuth.getInstance().getCurrentUser().id);
-            final DatabaseReference currentUserFollowRef = currentUserRef.child(FOLOWING_FRIEND_LIST);
-            final String receiveName = summaryReceive.first_name + summaryReceive.last_name;
-
-            currentUserFollowRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    if (dataSnapshot.child(summaryReceive.id).exists()) {
-                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
-                        alertDialogBuilder.setTitle("Error");
-                        alertDialogBuilder
-                                .setMessage("You already followed " + receiveName + "!")
-                                .setCancelable(false)
-                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int id) {
-                                        dialog.cancel();
-                                    }
-                                });
-                        AlertDialog alertDialog = alertDialogBuilder.create();
-                        alertDialog.show();
-                    } else {
-                        currentUserRef.child(FOLOWING_FRIEND_LIST).child(summaryReceive.id).setValue(summaryReceive);
-                        userTableRef.child(summaryReceive.id).child(FOLLOWER_LIST).child(UserAuth.getInstance().getCurrentUser().id).setValue(UserAuth.getInstance().getCurrentUserSummary());
-                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
-                        alertDialogBuilder.setTitle("Success");
-                        alertDialogBuilder
-                                .setMessage("You are now following " + receiveName + "!")
-                                .setCancelable(false)
-                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int id) {
-                                        dialog.cancel();
-                                    }
-                                });
-                        AlertDialog alertDialog = alertDialogBuilder.create();
-                        alertDialog.show();
-                    }
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                }
-            });
-        } else {
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
-            alertDialogBuilder.setTitle("Error");
-            alertDialogBuilder
-                    .setMessage("You can't follow user whose profile is not public!")
-                    .setCancelable(false)
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            dialog.cancel();
-                        }
-                    });
-            AlertDialog alertDialog = alertDialogBuilder.create();
-            alertDialog.show();
-        }
-    }
+//    public void followPerson(final UserSummary summaryReceive) {
+//        if (summaryReceive.status == 2) {
+//            final DatabaseReference userTableRef = FirebaseDatabase.getInstance().getReference().child(USERS_TABLE);
+//            final DatabaseReference currentUserRef = userTableRef.child(UserAuth.getInstance().getCurrentUser().id);
+//            final DatabaseReference currentUserFollowRef = currentUserRef.child(FOLOWING_FRIEND_LIST);
+//            final String receiveName = summaryReceive.first_name + summaryReceive.last_name;
+//
+//            currentUserFollowRef.addListenerForSingleValueEvent(new ValueEventListener() {
+//                @Override
+//                public void onDataChange(DataSnapshot dataSnapshot) {
+//                    if (dataSnapshot.child(summaryReceive.id).exists()) {
+//                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+//                        alertDialogBuilder.setTitle("Error");
+//                        alertDialogBuilder
+//                                .setMessage("You already followed " + receiveName + "!")
+//                                .setCancelable(false)
+//                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                                    public void onClick(DialogInterface dialog, int id) {
+//                                        dialog.cancel();
+//                                    }
+//                                });
+//                        AlertDialog alertDialog = alertDialogBuilder.create();
+//                        alertDialog.show();
+//                    } else {
+//                        currentUserRef.child(FOLOWING_FRIEND_LIST).child(summaryReceive.id).setValue(summaryReceive);
+//                        userTableRef.child(summaryReceive.id).child(FOLLOWER_LIST).child(UserAuth.getInstance().getCurrentUser().id).setValue(UserAuth.getInstance().getCurrentUserSummary());
+//                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+//                        alertDialogBuilder.setTitle("Success");
+//                        alertDialogBuilder
+//                                .setMessage("You are now following " + receiveName + "!")
+//                                .setCancelable(false)
+//                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                                    public void onClick(DialogInterface dialog, int id) {
+//                                        dialog.cancel();
+//                                    }
+//                                });
+//                        AlertDialog alertDialog = alertDialogBuilder.create();
+//                        alertDialog.show();
+//                    }
+//                }
+//
+//                @Override
+//                public void onCancelled(DatabaseError databaseError) {
+//                }
+//            });
+//        } else {
+//            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+//            alertDialogBuilder.setTitle("Error");
+//            alertDialogBuilder
+//                    .setMessage("You can't follow user whose profile is not public!")
+//                    .setCancelable(false)
+//                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                        public void onClick(DialogInterface dialog, int id) {
+//                            dialog.cancel();
+//                        }
+//                    });
+//            AlertDialog alertDialog = alertDialogBuilder.create();
+//            alertDialog.show();
+//        }
+//    }
 }
---
+++ app/src/main/java/cmpe/sjsu/socialawesome/Utils/FriendUtils.java
@@ -0,0 +1,115 @@
+package cmpe.sjsu.socialawesome.Utils;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import cmpe.sjsu.socialawesome.models.UserSummary;
+
+import static cmpe.sjsu.socialawesome.StartActivity.USERS_TABLE;
+import static cmpe.sjsu.socialawesome.models.User.FOLLOWER_LIST;
+import static cmpe.sjsu.socialawesome.models.User.FOLOWING_FRIEND_LIST;
+import static cmpe.sjsu.socialawesome.models.User.PENDING_FRIEND_LIST;
+import static cmpe.sjsu.socialawesome.models.User.WAITING_FRIEND_LIST;
+
+/**
+ * Created by bing on 5/11/17.
+ */
+
+public class FriendUtils {
+
+    //type: 0-friend, 1-follow
+    public static void addFriend(final Context context, int type, final UserSummary summaryReceive) {
+        String nodeSent = null;
+        String nodeReceive = null;
+        String dialogSuccess = null;
+        String dialogDuplicate = null;
+        String dialogPrivate = null;
+        switch (type) {
+            case 0:
+                nodeSent = PENDING_FRIEND_LIST;
+                nodeReceive = WAITING_FRIEND_LIST;
+                dialogDuplicate = "You are already friend with ";
+                dialogSuccess = "You are now friend with ";
+                dialogPrivate = "You can't add user whose profile is not public as friend!";
+                break;
+            case 1:
+                nodeSent = FOLOWING_FRIEND_LIST;
+                nodeReceive = FOLLOWER_LIST;
+                dialogDuplicate = "You already followed ";
+                dialogSuccess = "You are now following ";
+                dialogPrivate = "You can't follow user whose profile is not public!";
+                break;
+            default:
+        }
+        if (summaryReceive.status == 2) {
+            final DatabaseReference userTableRef = FirebaseDatabase.getInstance().getReference().child(USERS_TABLE);
+            final DatabaseReference currentUserRef = userTableRef.child(UserAuth.getInstance().getCurrentUser().id);
+            final DatabaseReference currentUserFollowRef = currentUserRef.child(nodeSent);
+            final DatabaseReference followerRef = userTableRef.child(summaryReceive.id).child(nodeReceive);
+            final String receiveName = summaryReceive.first_name + summaryReceive.last_name;
+            final String dialogDuFinal = dialogDuplicate;
+            final String dialogSuFinal = dialogSuccess;
+
+            currentUserFollowRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.child(summaryReceive.id).exists()) {
+                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                        alertDialogBuilder.setTitle("Error");
+                        alertDialogBuilder
+                                .setMessage(dialogDuFinal + receiveName + "!")
+                                .setCancelable(false)
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                        AlertDialog alertDialog = alertDialogBuilder.create();
+                        alertDialog.show();
+                    } else {
+                        currentUserFollowRef.child(summaryReceive.id).setValue(summaryReceive);
+                        followerRef.child(UserAuth.getInstance().getCurrentUser().id).setValue(UserAuth.getInstance().getCurrentUserSummary());
+                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                        alertDialogBuilder.setTitle("Success");
+                        alertDialogBuilder
+                                .setMessage(dialogSuFinal + receiveName + "!")
+                                .setCancelable(false)
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                        AlertDialog alertDialog = alertDialogBuilder.create();
+                        alertDialog.show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+        } else {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+            alertDialogBuilder.setTitle("Error");
+            alertDialogBuilder
+                    .setMessage(dialogPrivate)
+                    .setCancelable(false)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
+    }
+
+
+}
