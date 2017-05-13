cfd3a24580b7c7e2c549d4ca9707cf651a3fee9d
Lam Tran <lam.tran@sjsu.edu>
2017-05-12 17:07:29 -0700

    finishing email

--- app/src/main/java/cmpe/sjsu/socialawesome/Utils/HTTPUtil.java
+++ app/src/main/java/cmpe/sjsu/socialawesome/Utils/HTTPUtil.java
@@ -25,28 +25,33 @@ import cmpe.sjsu.socialawesome.models.PushMessageResquest;
 
 public class HTTPUtil {
     private static final String PUSH_NOTIFICATION_URL = "https://us-central1-social-awesome.cloudfunctions.net/sendPushNotificationToDevices";
+    private static final String EMAIL_URL = "https://us-central1-social-awesome.cloudfunctions.net/sendEmailToUser";
     private static RequestQueue smRequestQueue;
 
     public static void sendPushNotification(Context context, List<String> tokens, String title, String message, Map<String, String> data) {
-        sendPushNotification(context, new PushMessageResquest(tokens, title, message, data));
+        sendPushNotification(context, new PushMessageResquest(tokens, title, message, data), true);
     }
 
-    static void sendPushNotification(final Context context, PushMessageResquest resquest) {
+    public static void sendEmail(Context context, List<String> tokens, String title, String message) {
+        sendPushNotification(context, new PushMessageResquest(tokens, title, message, null), false);
+    }
+
+    static void sendPushNotification(final Context context, PushMessageResquest resquest, final boolean isNotification) {
         if (smRequestQueue == null) {
             smRequestQueue = Volley.newRequestQueue(context);
         }
 
         try {
             final JSONObject jsonBody = new JSONObject(new Gson().toJson(resquest));
-            smRequestQueue.add(new JsonObjectRequest(PUSH_NOTIFICATION_URL, jsonBody, new Response.Listener<JSONObject>() {
+            smRequestQueue.add(new JsonObjectRequest(isNotification ? PUSH_NOTIFICATION_URL : EMAIL_URL, jsonBody, new Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject response) {
-                    Toast.makeText(context, context.getString(R.string.success_send_push), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, isNotification ? context.getString(R.string.success_send_push) : context.getString(R.string.success_send_email), Toast.LENGTH_SHORT).show();
                 }
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(context, context.getString(R.string.failure_send_push), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, isNotification ? context.getString(R.string.failure_send_push) : context.getString(R.string.failure_send_email), Toast.LENGTH_SHORT).show();
                 }
             }));
         } catch (JSONException e) {
